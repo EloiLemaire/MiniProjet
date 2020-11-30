027528184d9f762c411099be1aef24a1ee453c40
Eloi Lemaire <lemaire.eloi@gmail.com>
2020-11-30 11:53:33 +0100

    Initial Commit

---
+++ MiniProjet/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MiniProjet
+//
+//  Created by Eloi Lemaire on 30/11/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
